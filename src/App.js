aaab66884dfc1a8e04a79a9a36364e617e2307c6
Chanchal Kumar Mishra <chanchalmishra1947@gmail.com>
2021-12-18 12:00:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const initialState = { data: [], timestamp: "" }
+  const [passwords, setPasswords] = useState(initialState);
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("http://localhost:5000/", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        }
+      });
+      let newpasswords = await res.json() || passwords;
+      newpasswords.data.sort((a, b) => a.id - b.id)
+      setPasswords({ data: newpasswords.data, timestamp: newpasswords.timestamp })
+    }
+    fetchData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar">
+        <h3>Password Generator</h3>
+        <div className="nav-timestanm">
+          <strong>Timestamp:</strong> {passwords.timestamp}
+        </div>
+      </div>
+      <div className="main">
+        <div className="box">
+          <div className='header'><div className='element'>
+            <div className="id">S. No.</div>
+            <div className="password">Password</div>
+          </div></div>
+          {passwords.data.map((e, key) =>
+            <div key={key} className='element'>
+              <div className="id">{e.id}</div>
+              <div className="password">{e.password}</div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
